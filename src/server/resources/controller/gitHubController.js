f6e0eeba593733634305fbea3db0852a0be42052
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-09-13 17:38:24 -0700

    Modularize code github

--- src/server/resources/controller/gitHubController.js
+++ src/server/resources/controller/gitHubController.js
@@ -1,8 +1,25 @@
-// // controller coder here
-// var forkchop = require('hackreactor-labs/utils.forkchop');
+// exports = module.exports = {}; // use exports for mutiples - module.exports for single functions
+var router = require('express').Router();
+const exec = require('child_process').exec;
+// pqoueythnsjhfk
 
+module.exports = function(namesList) {
+  console.log(`namesList in gitHubController = ${namesList}`);
+  // exec('echo "hello" >> ./resources/controller/pqoueythnsjhfk3.txt', (error, stdout, stderr) => {  // works 
+  exec(`forkchop tmr hackreactor fulcrum-students ${namesList}`, (error, stdout, stderr) => {
+    console.log('inside .exec');
+    if (error) {
+      console.error(`Brandon, you have an error - exec error: ${error}`);
+      return;
+    }
+    console.log('inside non-error part of exec');
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
+}
 
-
-
-// tmr <orgname> <teamname> [membersList]
-// tmr <reactorcore> <hackreactor> [whitespace-separated list of usernames - not comma spaced]
+/*
+exec("cat example.txt | forkchop tmr hackreactor fulcrum-students")
+"tmr hackreactor fulcrum-students 'member1 member2'"
+"echo 'member1 member2' | forkchop tmr hackreactor fulcrum-students"
+*/
--- src/server/resources/controller/index.js
+++ src/server/resources/controller/index.js
@@ -1,20 +1,13 @@
 var router = require('express').Router();
 const exec = require('child_process').exec;
+var gitHubRepoKiller = require('./gitHubController.js');
+// pqoueythnsjhfk
 
 router.route('/')
   .post((req, res) => {
     console.log('console inside constroller/index.js');
     console.log('req.body = ', req.body);
-    exec('cat /Users/brandoncourtney/Desktop/test/file1.txt > /Users/brandoncourtney/Desktop/test/file3.txt', (error, stdout, stderr) => {
-      console.log('inside .exec');
-      if (error) {
-        console.error(`Brandon, you have an error - exec error: ${error}`);
-        return;
-      }
-      console.log('inside non-error part of exec');
-      console.log(`stdout: ${stdout}`);
-      console.log(`stderr: ${stderr}`);
-    });
+    gitHubRepoKiller(req.body.githubHandle);
     res.status(200).json('res.status from constroller/index.js!!')
   })
 
