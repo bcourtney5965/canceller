a7d34bc4f5994d7bc105cccb6a0b757628dacfeb
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-09-18 16:50:29 -0700

    Make minor tweaks

--- auth.js
+++
@@ -1,8 +0,0 @@
-// // var exports = module.exports = {};
-
-// const gitHubAuth = {
-//   clientID: '9651ecb7e19',
-//   clientSecret: 'dc270c5ea06483a'
-// }
-
-// module.exports.gitHubAuth = gitHubAuth;
