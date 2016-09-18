4ca91f49dac12e14834f2d279a20519942fad354
Brandon Courtney <brandon.h.courtney@gmail.com>
2016-09-18 15:40:11 -0700

    Add code to help with installing forkchop

---
+++ installforkchop.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/reactorcore/utils.forkchop.git
+cd utils.forkchop/
+npm link
+cp config.json.example config.json
+echo Dear Wonderful User\, Please Put Your Github Token in this File.
+forkchop c
